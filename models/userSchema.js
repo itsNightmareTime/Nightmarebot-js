6791ad5aceb90a01097274f3a6f139e300a617ba
harrisonarmstrong93@gmail.com <harrisonarmstrong93@gmail.com>
2022-04-15 01:22:43 -0400

    Added a User Model for the MongoDb

---
+++ models/userSchema.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const { Schema, Model } = mongoose;
+
+const userSchema = new Schema(
+    {
+        steamID: {
+            type: Number,
+            required: 'Please Provide a SteamID64',
+            length: 17
+        },
+        userName: {
+            type: String,
+            required: 'Please Provide a userName'.
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false1
+    }
+);
+
+const User = model('User', userSchema);
+
+module.exports = User;
