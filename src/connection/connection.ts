96975150273cd8bd6cd13017f0297137d65f2cbd
Harrison Armstrong <harmstrong@whisker.com>
2025-04-18 20:02:12 -0400

    refactor database connection to use mongoose

--- src/connection/connection.ts
+++ src/connection/connection.ts
@@ -1,13 +1,11 @@
-import { Sequelize } from 'sequelize';
+import mongoose from 'mongoose'
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const database = new Sequelize(
-	process.env.DB_NAME ? process.env.DB_NAME : '',
-	process.env.DB_USER_NAME ? process.env.DB_USER_NAME : '',
-	process.env.DB_USER_PASSWORD ? process.env.DB_USER_PASSWORD : '', 
-	{
-		dialect: 'mysql',
-		logging: false,
-	}
-);
+export const database = mongoose.connect(
+  process.env.DB_URL as string,
+  {
+    user: process.env.DB_USER as string,
+    pass: process.env.DB_PASSWORD as string,
+  }
+)
